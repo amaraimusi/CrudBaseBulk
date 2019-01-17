9a4cf6c8f212d693c572866cc36cffa6003403c5
user <user@user-PC>
2019-01-17 22:00:57 +0900

    データを更新

---
+++ shell/13_export_db.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/sh
+
+echo '作業ディレクトリ'
+pwd
+
+echo "ローカルDBのパスワードを入力してください"
+read pw
+
+echo 'SQLをエクスポートします。'
+mysqldump -uroot -p$pw crud_base_bulk > crud_base_bulk.sql
+echo 'エクスポートしました。'
+
+
+echo "------------ 終わり"
+cmd /k
