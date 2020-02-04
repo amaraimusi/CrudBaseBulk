def7a2bf368402a6bc9a500fb08b9a99aa982e06
user <amaraimusi@gmail.com>
2020-02-04 14:53:43 +0900

    データを更新

---
+++ shell/01_upload.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/sh
+echo 'ソースコードを差分アップロードします。'
+
+rsync -auvz ../app amaraimusi@amaraimusi.sakura.ne.jp:www/CrudBaseBulk
+rsync -auvz ../doc amaraimusi@amaraimusi.sakura.ne.jp:www/CrudBaseBulk
+rsync -auvz ../shell amaraimusi@amaraimusi.sakura.ne.jp:www/CrudBaseBulk
+
+echo "------------ 送信完了"
+#cmd /k
